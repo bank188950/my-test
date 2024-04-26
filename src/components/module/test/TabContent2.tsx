29e000b40019bc33cc2172b1068628d28a8f69db
cherfulbank@gmail.com <bankkub>
2024-04-27 01:35:26 +0700

    Fixed: Tab2 get api

--- src/components/module/test/TabContent2.tsx
+++ src/components/module/test/TabContent2.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 
+type HairColorSummary = {
+  Black: number;
+  Blond: number;
+  Chestnut: number;
+  Brown: number;
+};
+
+type Department = {
+  male: number;
+  female: number;
+  ageRange: { min: number; max: number };
+  hair: HairColorSummary;
+  addressUser: Record<string, string>;
+};
+
+type DepartmentData = {
+  [department: string]: Department | {};
+}[];
+
 function TabContent2() {
   const { data, isLoading, isError } = useQuery({
     queryKey: ['departmentDetail'],
     queryFn: function () {
       return fetchData(`https://dummyjson.com/users`);
-      //return fetchData(`https://jsonplaceholder.typicode.com/posts`);
     },
     staleTime: 0,
   });
@@ -22,24 +40,109 @@ function TabContent2() {
     return data;
   }
 
-  if (data) console.log('data:', data);
+  let showData: React.ReactNode = '';
+  if (data) {
+    // Get name department
+    const arrDepartment: string[] = [];
+    data.users.forEach((user: any) => {
+      const department = user.company.department;
+
+      if (!arrDepartment.includes(department)) {
+        arrDepartment.push(department);
+      }
+    });
+
+    // Create structure department
+    let employeeData: DepartmentData = [];
+    for (let i = 0; i < arrDepartment.length; i++) {
+      employeeData.push({
+        [arrDepartment[i]]: {
+          male: 0,
+          female: 0,
+          ageRange: { min: 0, max: 0 },
+          hair: { Black: 0, Blond: 0, Chestnut: 0, Brown: 0 },
+        },
+      });
+    }
+
+    // Cal data department
+    data.users.forEach((user: any) => {
+      const department = user.company.department;
+
+      if (user.gender === 'male') {
+        (employeeData[arrDepartment.indexOf(department)][department] as Department).male += 1;
+      }
+
+      if (user.gender === 'female') {
+        (employeeData[arrDepartment.indexOf(department)][department] as Department).female += 1;
+      }
+
+      if (user.hair.color === 'Black') {
+        (employeeData[arrDepartment.indexOf(department)][department] as Department).hair.Black += 1;
+      }
+
+      if (user.hair.color === 'Blond') {
+        (employeeData[arrDepartment.indexOf(department)][department] as Department).hair.Blond += 1;
+      }
+
+      if (user.hair.color === 'Chestnut') {
+        (employeeData[arrDepartment.indexOf(department)][department] as Department).hair.Chestnut += 1;
+      }
+
+      if (user.hair.color === 'Brown') {
+        (employeeData[arrDepartment.indexOf(department)][department] as Department).hair.Brown += 1;
+      }
+
+      const age = user.age;
+      if (
+        age < (employeeData[arrDepartment.indexOf(department)][department] as Department).ageRange.min ||
+        (employeeData[arrDepartment.indexOf(department)][department] as Department).ageRange.min === 0
+      ) {
+        (employeeData[arrDepartment.indexOf(department)][department] as Department).ageRange.min = age;
+      }
+
+      if (
+        age > (employeeData[arrDepartment.indexOf(department)][department] as Department).ageRange.max ||
+        (employeeData[arrDepartment.indexOf(department)][department] as Department).ageRange.max === 0
+      ) {
+        (employeeData[arrDepartment.indexOf(department)][department] as Department).ageRange.max = age;
+      }
+    });
+
+    showData = employeeData.map((department) => {
+      const departmentName = Object.keys(department)[0];
+      const { male, female, hair, ageRange } = department[departmentName] as Department;
+      const { Black, Blond, Chestnut, Brown } = hair;
+      const { min, max } = ageRange;
+
+      return (
+        <div key={departmentName} className="text-base border border-solid p-2">
+          <h4 className="text-lg font-bold">{departmentName}</h4>
+          <div>
+            <div>
+              <span>Male</span> - {male}
+            </div>
+            <div>
+              <span>Female</span> - {female}
+            </div>
+            <div>
+              <span>ageRange</span> {min}-{max}
+            </div>
+            <div>
+              <span>Hair</span> - Black: {Black}, Blond: {Blond}, Chestnut: {Chestnut}, Brown: {Brown}
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
 
   return (
     <div className="text-lg font-normal">
       <h2>Create data from API</h2>
       {isLoading && <div>Loading...</div>}
       {isError && <div>Error fetch data</div>}
-      {/* {data && (
-        <div>
-          {data.map((item: any) => (
-            <div key={item.id}>
-              <p>
-                {item.id} {item.title}
-              </p>
-            </div>
-          ))}
-        </div>
-      )} */}
+      <div className="mt-2">{data && showData}</div>
     </div>
   );
 }
